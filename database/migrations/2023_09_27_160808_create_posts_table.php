054b22f9f650611f532437899427e873fff0cdd6
Delos-343 <mfachry.handoko@gmail.com>
2023-09-27 23:13:24 +0700

    added new schema

--- database/migrations/2023_09_27_160808_create_posts_table.php
+++ database/migrations/2023_09_27_160808_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('title');
+            $table->string('image');
+            $table->text('content');
             $table->timestamps();
         });
     }
