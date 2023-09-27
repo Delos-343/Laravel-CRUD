2f403eca1d889bbb2a41c21bdb4a45ced204dd48
Delos-343 <mfachry.handoko@gmail.com>
2023-09-28 01:22:47 +0700

    completed PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'title' => 'required|string',
+            'image' => 'required|image',
+            'content' => 'required|string',
+        ]);
+
+        $imagePath = $request->file('image')->store('uploads', 'public');
+
+        Post::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'title' => $data['title'],
+            'image' => $imagePath,
+            'content' => $data['content'],
+        ]);
+
+        return redirect()->route('posts.index');
+    }
 }
