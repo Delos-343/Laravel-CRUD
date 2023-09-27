c5ebba23e3db3b71ed420ed6e34f0664c942f833
Delos-343 <mfachry.handoko@gmail.com>
2023-09-28 01:50:36 +0700

    styled the Create and Index views

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,45 +2,47 @@
 
 @section('content')
 <div class="container mx-auto mt-8">
-    <h1 class="text-2xl font-semibold mb-4">
+    <h1 class="text-3xl font-semibold text-center text-blue-500 mb-8">
         Create Post
     </h1>
     <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="mb-4">
-            <label for="name" class="block font-medium">
-                Name
-            </label>
-            <input type="text" name="name" id="name" class="form-input w-full" required>
-        </div>
-        <div class="mb-4">
-            <label for="email" class="block font-medium">
-                Email
-            </label>
-            <input type="email" name="email" id="email" class="form-input w-full" required>
-        </div>
-        <div class="mb-4">
-            <label for="title" class="block font-medium">
-                Title
-            </label>
-            <input type="text" name="title" id="title" class="form-input w-full" required>
-        </div>
-        <div class="mb-4">
-            <label for="image" class="block font-medium">
-                Image
-            </label>
-            <input type="file" name="image" id="image" class="form-input w-full" required>
-        </div>
-        <div class="mb-4">
-            <label for="content" class="block font-medium">
-                Content
-            </label>
-            <textarea name="content" id="content" class="form-textarea w-full" required></textarea>
-        </div>
-        <div class="mb-4">
-            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                Post
-            </button>
+        <div class="bg-opacity-50 bg-black rounded-lg p-4 text-white border border-gray-600 shadow-md hover:shadow-lg hover:bg-blue-600 transition duration-300 ease-in-out transform hover:-translate-y-2">
+            <div class="mb-4">
+                <label for="name" class="block text-white font-medium mb-2">
+                    Name
+                </label>
+                <input type="text" name="name" id="name" class="form-input w-full bg-transparent text-white border-b-2 border-gray-500" required>
+            </div>
+            <div class="mb-4">
+                <label for="email" class="block text-white font-medium mb-2">
+                    Email
+                </label>
+                <input type="email" name="email" id="email" class="form-input w-full bg-transparent text-white border-b-2 border-gray-500" required>
+            </div>
+            <div class="mb-4">
+                <label for="title" class="block text-white font-medium mb-2">
+                    Title
+                </label>
+                <input type="text" name="title" id="title" class="form-input w-full bg-transparent text-white border-b-2 border-gray-500" required>
+            </div>
+            <div class="mb-4">
+                <label for="image" class="block text-white font-medium mb-2">
+                    Image
+                </label>
+                <input type="file" name="image" id="image" class="form-input w-full bg-transparent text-white border-b-2 border-gray-500" required>
+            </div>
+            <div class="mb-4">
+                <label for="content" class="block text-white font-medium mb-2">
+                    Content
+                </label>
+                <textarea name="content" id="content" class="form-textarea w-full bg-transparent text-white border-b-2 border-gray-500" required></textarea>
+            </div>
+            <div class="mb-4">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full hover:shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1">
+                    Post
+                </button>
+            </div>
         </div>
     </form>
 </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 <div class="container mx-auto mt-8">
-    <h1 class="text-2xl font-semibold mb-4">Posts</h1>
-    <ul>
+    <h1 class="text-3xl font-semibold text-center text-blue-500 mb-8">
+        Posts
+    </h1>
+    <ul class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         @foreach($posts as $post)
-            <li>
-                <h2 class="text-lg font-semibold">{{ $post->title }}</h2>
-                <p>{{ $post->content }}</p>
+            <li class="bg-opacity-50 bg-black rounded-lg p-4 text-white border border-gray-600 shadow-md hover:shadow-lg hover:bg-blue-600 transition duration-300 ease-in-out transform hover:-translate-y-2">
+                <h2 class="text-xl font-semibold">{{ $post->title }}</h2>
+                <p class="mt-2 text-gray-300">{{ $post->content }}</p>
             </li>
         @endforeach
     </ul>
